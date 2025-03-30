79e9ba54434cd66fef0abb5ff47cc532db9ed9a0
enascime <edjunior.nascimento@capgemini.com>
2025-03-30 01:44:32 -0300

    feat(protecao-animal): inicio do desenvolvimento tela do projeto protecao animal

--- script.js
+++ script.js
@@ -12,11 +12,3 @@ function toggleMenu() {
     }
   
 }
-
-const menuItems = document.querySelectorAll('.menu-item');
-menuItems.forEach(item => {
-    item.addEventListener('click', function() {
-        menuItems.forEach(link => link.classList.remove('selecionado'));
-        this.classList.add('selecionado');
-    });
-});
